dff990dbfb03d0ddfa8f13b96f60e7841c22eeb4
Unknown <rizvisfbrina@gmail.com>
2018-12-17 20:27:23 +0700

    ganti

--- onlykeu/list.php
+++ onlykeu/list.php
@@ -455,7 +455,7 @@ $(document).ready(function(){
       <div class="modal-content">
         <form>
           <div class="modal-header">            
-            <h4 class="modal-title">Edit Employee</h4>
+            <h4 class="modal-title">Edit Pembayaran</h4>
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           </div>
           <div class="modal-body">          
